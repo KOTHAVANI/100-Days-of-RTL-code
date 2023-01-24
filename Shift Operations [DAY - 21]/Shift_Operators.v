e3493e8a10506cd31d7367a5fb163d099bcb17eb
KOTHAVANI <121639288+KOTHAVANI@users.noreply.github.com>
2023-01-24 22:56:37 +0530

    Update Shift_Operators.v

--- Shift Operations [DAY - 21]/Shift_Operators.v
+++ Shift Operations [DAY - 21]/Shift_Operators.v
@@ -9,10 +9,10 @@ wire [1:0] sel;
 always @ (in or sel)
 begin
 case ( sel )
-  2'b00 : out = in << 1;
-  2'b01 : out = in >> 1;
-  2'b10 : out = in <<< 1;
-  2'b11 : out = in >>> 1;
+  2'b00 : out = in << 1;  //Logical left shift
+  2'b01 : out = in >> 1;  //Logical right shift
+  2'b10 : out = in <<< 1; //Arithmetic left shift
+  2'b11 : out = in >>> 1; //Arithmetic right shift
 default : out = 4'b0000;
 endcase
 end
