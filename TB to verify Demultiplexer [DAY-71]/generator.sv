c2fd3c22c7d5e9eb2d9e1661fb40abbd81dd4eb9
Vani Kotha <121639288+KOTHAVANI@users.noreply.github.com>
2023-04-17 22:09:56 +0530

    Create generator.sv

---
+++ TB to verify Demultiplexer [DAY-71]/generator.sv
@@ -0,0 +1,17 @@
+class generator;
+transaction tr;
+mailbox mbox;
+function new(mailbox mbox);
+this.mbox= mbox;
+endfunction
+task run();
+  repeat(20) begin
+tr=new();
+tr.randomize();
+
+    $display("D=%b Sel=%b", tr.d, tr.sel);
+    
+mbox.put(tr);
+end
+endtask
+endclass
